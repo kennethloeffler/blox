8be20a6ed0a037cd13192487163929783c429a17
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2021-02-13 16:48:55 -0800

    Bump version

--- blox.el
+++ blox.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2021 Kenneth Loeffler
 
 ;; Author: Kenneth Loeffler <kenneth.loeffler@outlook.com>
-;; Version: 0.0.0
+;; Version: v0.0.1
 ;; Keywords: roblox, rojo, tools
 ;; URL: https://github.com/kennethloeffler/blox
 ;; Package-Requires: ((emacs "25.1"))
